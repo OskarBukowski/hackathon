02e656f1b300aed78752c1d47d9acdf42238f8a4
ccxAlpha <oskar.bukowski1@gmail.com>
2022-03-20 00:58:58 +0100

    .

--- FlaskApiServerAws.py
+++ FlaskApiServerAws.py
@@ -1,27 +1,23 @@
 from flask import Flask, request, jsonify, escape
 from ClientMongoDB import MongoDBClient
-from flask_sslify import SSLify
+from bson.json_util import dumps
 import requests
 import json
 
 app = Flask(__name__)
 
 
-# {"id": 12345678, "model_output": 1.0}
-
 @app.route('/id/<int:tweet_id>', methods=['GET'])
 def post_request(tweet_id):
-    # if exist in database
     response = MongoDBClient().read_db({"id": tweet_id})
     if response is None:
-        resp = requests.get(f"https://fdd3-34-136-46-201.ngrok.io/?id={tweet_id}&fbclid=IwAR0OGQZ25H5zaBptAhLh7nIKBqWSD5cAnhp4p6BUPYBQK9D-ZMD1N-Shezs")
-        prob = resp.json()['prob']
-        response = {"id": tweet_id, "model_output": prob}
+        resp = requests.get("https://fdd3-34-136-46-201.ngrok.io/?id={}&fbclid=IwAR0OGQZ25H5zaBptAhLh7nIKBqWSD5cAnhp4p6BUPYBQK9D-ZMD1N-Shezs".format(tweet_id))
+        response = {str(tweet_id): resp.json()["prob"]}
         MongoDBClient().update_db(response)
-        return jsonify(response)
+        return json.loads(dumps({str(tweet_id): resp.json()["prob"]}))
 
     else:
-        return jsonify({response['id']: response['model_output']})
+        return jsonify({response["id"]: response["model_output"]})
 
 
 if __name__ == '__main__':
