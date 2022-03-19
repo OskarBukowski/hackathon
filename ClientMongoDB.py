0f49fea3ce35245e73d6f4f28f4f4698d463bfbf
ccxAlpha <oskar.bukowski1@gmail.com>
2022-03-19 19:41:20 +0100

    added test Flask HTTP server

--- ClientMongoDB.py
+++ ClientMongoDB.py
@@ -19,13 +19,13 @@ db.tweet_with_model_output.insert_one(insert)
 response = db.tweet_with_model_output.find_one({"id": 12345678})
 
 
-print(response)
+# print(response)
 
 
 class MongoDBClient:
     def __init__(self):
         self.db = MongoClient(
-            "mongodb+srv://obukowski:cKeZ8bMmChydetKt@cluster0.oblol.mongodb.net/main?retryWrites=true&w=majority").main.tweet_with_model_output
+            "mongodb+srv://obukowski:cKeZ8bMmChydetKt@cluster2.l4m5p.mongodb.net/main?retryWrites=true&w=majority").main.tweet_with_model_output
 
     def update_db(self, attribute):
         return self.db.insert_one(attribute)
@@ -38,10 +38,10 @@ id = {"id": 12345678}
 
 d = MongoDBClient().read_db(id)
 
-print(d)
+# print(d)
 
 # r = {'_id': ObjectId('6235ffdd6c156c9cc5707512'), 'id': 12345678, 'model_output': 1.0}
 
 response = {d['id']: d['model_output']}
 
-print(type(response))
+# print(type(response))
