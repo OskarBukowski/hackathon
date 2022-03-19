c91d5ed71d29ef421ebee4ddad660cd800312f13
ccxAlpha <oskar.bukowski1@gmail.com>
2022-03-19 17:11:23 +0100

    data labeling

--- ClientMongoDB.py
+++ ClientMongoDB.py
@@ -1,11 +1,22 @@
 from pymongo import MongoClient
 from pprint import pprint
 
+client = MongoClient(
+    "mongodb+srv://obukowski:cKeZ8bMmChydetKt@cluster0.oblol.mongodb.net/main?retryWrites=true&w=majority")
 
-client = MongoClient("mongodb+srv://obukowski:cKeZ8bMmChydetKt@cluster0.oblol.mongodb.net/test?retryWrites=true&w=majority")
-db = client.admin
-
-
+db = client.main
+collection = db.tweet_with_model_output
 serverStatusResult = db.command("serverStatus")
 
-pprint(serverStatusResult)
+# pprint(serverStatusResult)
+
+### EXAMPLE ENTRY
+
+insert = {"id": 12345678, "model_output": 1.0}
+
+# db.tweet_with_model_output.insert_one(insert)
+
+
+### FIND ENTRY BY ID
+fivestar = db.tweet_with_model_output.find_one({"id": 12345678})
+print(fivestar)
