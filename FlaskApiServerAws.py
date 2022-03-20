bec2d112775309d87ac5230f8fb6cc94eb431f87
ccxAlpha <oskar.bukowski1@gmail.com>
2022-03-20 10:06:45 +0100

    .

--- FlaskApiServerAws.py
+++ FlaskApiServerAws.py
@@ -12,7 +12,9 @@ def post_request(tweet_id):
     try:
         response = MongoDBClient().read_db({"id": str(tweet_id)})
         if response is None:
-            resp = requests.get("https://38f5-34-80-85-154.ngrok.io/?id={}&fbclid=IwAR1GFvGG-Ik_L2IwR9zE5upCG5UsaG19-Z_19YCAW_zxLRQJfYcuGacfMdY".format(tweet_id))
+            resp = requests.get(
+                "https://bef5-34-122-234-12.ngrok.io/?id={}&fbclid=IwAR35Wdjb4RAEPSBOYHfikQQ9JcgkYtUjan9W3OA_N-OC0nEChclQ648PGQ0".format(
+                    tweet_id))
 
             response = {"id": str(tweet_id), "model_output": resp.json()["prob"]}
             MongoDBClient().update_db(response)
